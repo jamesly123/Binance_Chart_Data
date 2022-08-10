9aa73d945eb69ac8b0b38cafdfde5e86888ee19a
JamesPoppyCock <42442664+jamesly123@users.noreply.github.com>
2022-08-11 00:13:22 +0800

    Automatic removal of previously downloaded files

--- download_chart_1h.py
+++ download_chart_1h.py
@@ -8,6 +8,7 @@ import os
 import glob
 import pandas as pd
 import csv
+from csv import writer
 
 #========================
 #Filename format: BTCUSDT-1h-yyyy-mm-dd.zip
@@ -30,28 +31,40 @@ print ("Files will be downloaded and extracted to current directory folder \"Dow
 curr_dir = os.getcwd()
 
 def dl_extract_hourly_data():
-    headerList = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'QAV', 'No. of trades', 'Taker buy base', 'Taker buy quote', 'Ignore']
-    
-    for x in range(3):
+	headerList = ['Open Time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'QAV', 'No. of trades', 'Taker buy base', 'Taker buy quote', 'Ignore']
+	
+	#Remove all files previously downloaded so it don't get appended into combined csv
+	print ('\nClearing directory of previous download files...\n')
+	try:
+		for filename in glob.glob(curr_dir + '\\Downloaded_Data\\BTCUSDT-1h*'):
+			os.remove(filename)
 
-        date_time_desc = current_datetime - timedelta(days = x+1)
-        date_time_desc_formatted = datetime.strftime(date_time_desc, "%Y-%m-%d")
-        filename_zip = 'BTCUSDT-1h-' + date_time_desc_formatted + '.zip'
-        filename_csv = 'BTCUSDT-1h-' + date_time_desc_formatted + '.csv'
-        url = 'https://data.binance.vision/data/spot/daily/klines/BTCUSDT/1h/' + filename_zip
-        #r = requests.get(url, allow_redirects=True)
+		os.remove(curr_dir + '\\Downloaded_Data\\Combined.csv')
+		print ('\nCleared...\n')
+	except:
+		pass
 
-        print ("Downloaded and extracting daily hourly data for " + date_time_desc_formatted)
-        
-        with urlopen(url) as zipresp:
-            with ZipFile(BytesIO(zipresp.read())) as zfile:
-                zfile.extractall(curr_dir + '\\Downloaded_Data')
-        
-        os.chdir(curr_dir + '\\Downloaded_Data')
-        
-        file = pd.read_csv(filename_csv)
-        file.to_csv(filename_csv, header=headerList, index=False)
-    
+	for x in range(3):
+
+		date_time_desc = current_datetime - timedelta(days = x+1)
+		date_time_desc_formatted = datetime.strftime(date_time_desc, "%Y-%m-%d")
+		filename_zip = 'BTCUSDT-1h-' + date_time_desc_formatted + '.zip'
+		filename_csv = 'BTCUSDT-1h-' + date_time_desc_formatted + '.csv'
+		url = 'https://data.binance.vision/data/spot/daily/klines/BTCUSDT/1h/' + filename_zip
+		#r = requests.get(url, allow_redirects=True)
+
+		print ("Download and extracting daily hourly data for " + date_time_desc_formatted)
+		
+		with urlopen(url) as zipresp:
+			with ZipFile(BytesIO(zipresp.read())) as zfile:
+				zfile.extractall(curr_dir + '\\Downloaded_Data')
+		
+		os.chdir(curr_dir + '\\Downloaded_Data')
+
+		#Read file and specify header, then append header the next. Else headerList will overwrite data in first row.
+		file = pd.read_csv(filename_csv, header=None)
+		file.to_csv(filename_csv, header=headerList, index=False)
+	
 '''
 Each Kline hourly CSV data starts from 12am, to 11pm of that day.
 
@@ -76,8 +89,8 @@ Binance Klines Header CSV:
 3) Column A into custom format (Date + time)
 5) Collect data from first moons, every cycle = 14 days? (Double check TV)
 4) Insert extra column
-    Column Name: New Moon/Full Moon
-    Values: Sell/Buy
+	Column Name: New Moon/Full Moon
+	Values: Sell/Buy
 5) Based on Moon cycle, fill in Sell/Buy on each new cycle.
 6) Calculate total profits if an individual were to buy/sell x hours after each cycle. Determine average.
 '''
@@ -85,18 +98,16 @@ Binance Klines Header CSV:
 #1
 #====
 def concat_csv():
-    
-
-    print ("\nDownload Completed. Combining all CSVs into \"Combined_csv.csv\"")
-    os.chdir(curr_dir + '\\Downloaded_Data')
-    
-    extension = 'csv'
-    all_filenames = glob.glob("*.csv")
-    #Combine all files in the list
-    
-    df = pd.concat(map(pd.read_csv, all_filenames), ignore_index=True)
-    df.to_csv("Combined.csv", index=False)
-    
+	print ("\nDownload Completed. Combining all CSVs into \"Combined_csv.csv\"")
+	os.chdir(curr_dir + '\\Downloaded_Data')
+	
+	extension = 'csv'
+	all_filenames = glob.glob("*.csv")
+	#Combine all files in the list
+	
+	df = pd.concat(map(pd.read_csv, all_filenames), ignore_index=True)
+	df.to_csv("Combined.csv", index=False)
+	
 #====================
 #Functions
 #====================
